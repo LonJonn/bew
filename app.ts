ca340fc192e9d286ac91d6f1de85c5bec0fa073e
LonJonn <leon.salsiccia@gmail.com>
2021-08-01 19:23:42 +1000

    port

--- app.ts
+++ app.ts
@@ -73,4 +73,6 @@ io.on("connection", function (socket) {
 
 app.use(express.static("public"));
 
-server.listen(3000, () => console.log("listening on port 3000"));
+server.listen(process.env.PORT || 3000, () =>
+  console.log("listening on port 3000")
+);
